a8dbb4b7f5dfd04338f9fd8af960aec9c3163dc4
Athos-afk <athos.lima1310@gmail.com>
2026-09-20 14:38:11 -0400

    Update q2.py

--- atividade2/q2.py
+++ atividade2/q2.py
@@ -1,10 +1,10 @@
-# Questão 2 - Tabuada[cite: 1]
+# Questão 2 - Tabuada
 
-# Solicita um número inteiro ao utilizador[cite: 1]
+# Solicita um número inteiro ao utilizador
 numero = int(input("Digite um número: "))
 
 print(f"\n--- Tabuada do {numero} ---")
-# Estrutura for para calcular a tabuada de 1 a 10[cite: 1]
+# Estrutura for para calcular a tabuada de 1 a 10
 for i in range(1, 11):
     resultado = numero * i
     print(f"{numero} x {i} = {resultado}")
