a44d8e189e9d2a4182ef21605e9dae0f032d27ba
Athos-afk <athos.lima1310@gmail.com>
2026-09-20 14:38:31 -0400

    Update q3.py

--- atividade2/q3.py
+++ atividade2/q3.py
@@ -1,10 +1,10 @@
-# Questão 3 - Soma dos Números[cite: 1]
+# Questão 3 - Soma dos Números
 
 soma_total = 0
 soma_pares = 0
 soma_impares = 0
 
-# Percorre os números de 1 a 50[cite: 1]
+# Percorre os números de 1 a 50
 for i in range(1, 51):
     soma_total += i
     if i % 2 == 0:
@@ -12,7 +12,7 @@ for i in range(1, 51):
     else:
         soma_impares += i
 
-# Apresenta os resultados finais[cite: 1]
+# Apresenta os resultados finais
 print("--- Resultados da Soma (1 a 50) ---")
 print(f"Soma de todos os números: {soma_total}")
 print(f"Soma dos números pares: {soma_pares}")
